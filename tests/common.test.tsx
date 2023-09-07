5c8936bed226bf47f4cb3c90fceee1a80de0a36b
Amirreza Pourkarim <amir.bolnik@gmail.com>
2023-09-07 13:20:28 +0330

    feat: little change

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -2,12 +2,10 @@ import * as React from 'react'
 import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
-import {MyCounter} from "../src";
-
-
+import { MyCounter } from '../src'
 
 describe('Common render', () => {
-    it('renders without crashing', () => {
-        render(<MyCounter />)
-    })
+  it('renders without crashing', () => {
+    render(<MyCounter />)
+  })
 })
